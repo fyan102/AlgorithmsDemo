e62e333ae06099a029bede99f7badf3793dab0fc
zllzfw <zllzfwcl@gmail.com>
2020-08-23 17:42:28 +1000

    code gen

--- src/org/fyan102/algorithms/ui/ClassGenerator.java
+++ src/org/fyan102/algorithms/ui/ClassGenerator.java
@@ -1,5 +1,30 @@
 package org.fyan102.algorithms.ui;
 
-public class ClassGenerator {
+import java.io.*;
 
+public class ClassGenerator {
+    public boolean generate(String code) {
+        try (PrintWriter writer = new PrintWriter("Heuristic.java")) {
+            writer.print(code);
+            writer.close();
+            Process p = Runtime.getRuntime().exec("cmd /c javac Heuristic.java");
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+
+            try {
+                while ((line = input.readLine()) != null)
+                    System.out.println(line);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 }
